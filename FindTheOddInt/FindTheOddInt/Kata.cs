d8f43e6bafe1b79d9f8269848da8ac2b9aabbe67
jason.huang22 <jason.huang@titansoft.com.sg>
2018-09-22 21:46:34 +0800

    remove static

--- FindTheOddInt/FindTheOddInt/Kata.cs
+++ FindTheOddInt/FindTheOddInt/Kata.cs
@@ -4,7 +4,7 @@ namespace FindTheOddInt
 {
     public class Kata
     {
-        public static int find_it(int[] seq)
+        public int find_it(int[] seq)
         {
             return seq.GroupBy(x => x).Where(y => y.Count() % 2 != 0).ToList()[0].Key;
         }
--- FindTheOddInt/FindTheOddInt/Program.cs
+++ FindTheOddInt/FindTheOddInt/Program.cs
@@ -10,7 +10,6 @@ namespace FindTheOddInt
     {
         static void Main(string[] args)
         {
-            Kata.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 });
         }
     }
 }
